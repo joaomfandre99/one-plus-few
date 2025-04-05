fe9c5175e812de282154a2f10f2834a4727b1594
JoaoMFAndre <joaoandre@dynamik.pt>
2025-04-05 13:00:56 +0100

    Initial commit

---
+++ Assets/Scripts/Helpers.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+public static class Helpers
+{
+    private static Matrix4x4 isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(this Vector3 input) => isoMatrix.MultiplyPoint3x4(input);
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float turnSpeed = 360;
+    private Vector3 input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Look();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Look()
+    {
+        if (input == Vector3.zero) return;
+        
+        var rotation = Quaternion.LookRotation(input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        rb.MovePosition(transform.position + (transform.forward * input.magnitude) * speed * Time.deltaTime);
+    }
+}
